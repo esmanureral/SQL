d7af69613f947e8e1ccfbc020aa225f7d91ec895
Esmanur <esmanur2eral@gmail.com>
2024-08-12 11:04:13 +0300

    101

--- İNSAN KAYNAKLARI VERİTABANI/Temel Sorgular/SQLQuery1.sql
+++ İNSAN KAYNAKLARI VERİTABANI/Temel Sorgular/SQLQuery1.sql
@@ -41,3 +41,41 @@ SELECT *,
 (SELECT COUNT(*) FROM PERSON WHERE DEPARTMENTID = D.ID AND GENDER = 'K' AND OUTDATE IS NULL) AS FEMALE_PERSONCOUNT
 FROM DEPARTMENT D
 ORDER BY D.DEPARTMENT
+
+-----------------------------------------------------------------------------------------
+
+--4.SORGU
+/*þirketimizin planlama departmanýna yeni bir þef atamasý yapýldý maaþý belirlemek istiyoruz.
+planlama departmaný için min,max,ort þef maaþý getiren sorgu (iþten çýkmýþlar dahil)*/
+
+SELECT 
+POS.DEPARTMENT,MIN(P.SALARY) AS MIN_SALARY,
+MAX(P.SALARY) AS MAX_SALARY,
+AVG(P.SALARY) AS AVG_SALARY
+FROM POSITION POS
+INNER JOIN PERSON P ON P.POSITIONID = POS.ID
+WHERE POS.DEPARTMENT = 'PLANLAMA ÞEFÝ'
+GROUP BY POS.DEPARTMENT
+
+
+SELECT 
+POS.DEPARTMENT,
+(SELECT MIN(SALARY) FROM PERSON WHERE POSITIONID=POS.ID) AS MIN_SALARY,
+(SELECT MAX(SALARY) FROM PERSON WHERE POSITIONID=POS.ID) AS MAX_SALARY,
+(SELECT ROUND(AVG(SALARY),0) FROM PERSON WHERE POSITIONID=POS.ID) AS AVG_SALARY
+FROM POSITION POS
+WHERE POS.DEPARTMENT='PLANLAMA ÞEFÝ'
+
+
+-----------------------------------------------------------------------------------------
+
+--5.SORGU
+/* Her bir pozisyonda mevcut halde çaliþanlar olarak kaç kiþi ve ortalama maaþlarýn
+ne kadar oldugunu listelettir*/
+
+SELECT POS.DEPARTMENT,
+(SELECT COUNT(*) FROM PERSON WHERE POSITIONID=POS.ID)AS PERSONCOUNT,
+(SELECT ROUND (AVG(SALARY),0) FROM PERSON WHERE POSITIONID=POS.ID)AS AVG_SALARY
+FROM POSITION POS 
+ORDER BY POS.DEPARTMENT
+
